35c732be451139b1f538e91d839dd92baa1d5241
kristijonas <kristijonasza@gmail.com>
2022-05-11 16:54:05 +0300

    created database migrations

---
+++ app/Database/Migrations/2022-05-10-164451_CreateStudentsTable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+
+            'id' => [
+                'type' => 'INT',
+                'auto_increment' => true,
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+            'class_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('students');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('students');
+    }
+}
